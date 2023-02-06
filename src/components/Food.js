a84aed0f89e81b5ef28f0d1d0faf829747fbf830
Michaela Fooksa <mfooksa@gmail.com>
2023-02-06 16:07:53 -0700

    set price to two decimal points

--- src/components/Food.js
+++ src/components/Food.js
@@ -2,7 +2,7 @@ import Price from './Price'
 import { useState } from 'react'
 
 const Food = (props) => {
-  const [isShown, setIsShown] = useState(false)
+  const [shown, setShown] = useState(false)
   //   const allergens = props.allergens
   //   let new_allergens=[]
   //   for (i=0;i<allergens.length;i++){
@@ -10,21 +10,19 @@ const Food = (props) => {
   // }
   const show = (e) => {
     e.preventDefault()
-    setIsShown((current) => !current)
+    setShown((current) => !current)
   }
   return (
     <div class="food">
       <h4>
         <a onClick={show}>{props.name}</a>
       </h4>
-      {isShown && (
+      {shown && (
         <div>
           <img src={props.image} />
         </div>
       )}
-      {/* <img src={props.image} />
-      <h5>Allergens: </h5> */}
-      <h5>Price: ${props.price}</h5>
+      <h5>Price: ${props.price.toFixed(2)}</h5>
     </div>
   )
 }
